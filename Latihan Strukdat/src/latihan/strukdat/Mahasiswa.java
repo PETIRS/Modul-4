adbb963d448aec7dfd9d7e553a03a295020289eb
PETIRS <127311053+PETIRS@users.noreply.github.com>
2023-05-11 06:59:03 +0700

    Add files via upload

---
+++ Latihan Strukdat/src/latihan/strukdat/Mahasiswa.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihan.strukdat;
+
+import java.util.Scanner;
+import java.util.HashMap;
+/**
+ *
+ * @author PETIRSON
+ */
+public class Mahasiswa {
+     String nama, kelas, matkulPraktikum;
+    int nim;
+
+    public Mahasiswa (String nm, String kl, String mat, int ni){
+        nama = nm;
+        kelas = kl;
+        matkulPraktikum = mat;
+        nim = ni;
+    }
+
+    public static void main(String[] args) {
+       Scanner in = new Scanner(System.in);
+        HashMap<String , Mahasiswa> mhs = new HashMap<>();
+        String input;
+        Mahasiswa data;
+
+        mhs.put("1", new Mahasiswa("Andi", "3H", "Struktur Data", 103703110));
+        mhs.put("2", new Mahasiswa("Budi", "3A", "Matematika Diskrit", 103703111));
+
+
+
+        System.out.print("MASUKKAN ID : ");
+        input= in.nextLine();
+        data = mhs.get(input);
+        if ((data != null)) {
+            System.out.println("Data Mahasiswa : "+data.nama +", kelas:"+ data.kelas+", mata kuliah praktikum :"+data.matkulPraktikum+", nim :"+data.nim);
+        }
+    }
+}
+    
+
